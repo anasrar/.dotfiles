da1d47e49c08ea8c1fa4d03180f71323dba2a33f
Anas RAR <mynameanasrar@gmail.com>
2022-06-07 13:18:15 +1200

    feat: replace default telescope sorter with fzy

--- neovim/.config/nvim/lua/finder/telescope.lua
+++ neovim/.config/nvim/lua/finder/telescope.lua
@@ -3,7 +3,14 @@
 - [ripgrep](github.com/BurntSushi/ripgrep) for live_grep
 --]=]
 
-require('telescope').setup({})
+require('telescope').setup({
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
+    }
+  }
+})
 require('telescope').load_extension('fzy_native')
 
 local map = vim.api.nvim_set_keymap
