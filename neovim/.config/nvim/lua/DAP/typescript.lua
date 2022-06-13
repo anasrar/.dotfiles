18f2a23aba5d0610fc50d50e52748f3edb91cc97
Anas RAR <mynameanasrar@gmail.com>
2022-06-14 00:18:52 +1200

    feat: add node2 adapter for DAP

--- neovim/.config/nvim/lua/DAP/typescript.lua
+++ neovim/.config/nvim/lua/DAP/typescript.lua
@@ -10,6 +10,16 @@ run chrome with argument `--remote-debugging-port=9222`
 ```
 chrome --remote-debugging-port=9222
 ```
+
+install vscode node debug2
+```
+git clone https://github.com/microsoft/vscode-node-debug2.git ~/.DAP/vscode-node-debug2
+cd ~/.DAP/vscode-node-debug2
+npm install
+NODE_OPTIONS=--no-experimental-fetch npm run build
+```
+and run any node using flag `--inspect` or `--inspect-brk`, you can also debug deno using the same flag
+
 --]=]
 
 local dap = require('dap')
@@ -18,6 +28,12 @@ dap.adapters.chrome = {
   type = 'executable',
   command = 'node',
   args = { os.getenv('HOME') .. '/.DAP/vscode-chrome-debug/out/src/chromeDebug.js' },
+}
+
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = { os.getenv('HOME') .. '/.DAP/vscode-node-debug2/out/src/nodeDebug.js' },
 }
 
 local exts = {'javascript','typescript','javascriptreact','typescriptreact'}
@@ -34,6 +50,12 @@ for i, ext in ipairs(exts) do
       protocol = 'inspector',
       port = 9222,
       webRoot = '${workspaceFolder}',
-    }
+    },
+    {
+      type = 'node2',
+      request = 'attach',
+      name = 'Attach Program (Node2)',
+      processId = require('dap.utils').pick_process,
+    },
   }
 end
