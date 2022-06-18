20bc45cc8cb90b52fb43cc660e79c9a11eee0611
Anas RAR <mynameanasrar@gmail.com>
2022-06-18 21:23:12 +1200

    chore: add tokyonight setting

--- neovim/.config/nvim/lua/theme/tokyonight.lua
+++ neovim/.config/nvim/lua/theme/tokyonight.lua
@@ -1,9 +1,19 @@
 vim.o.termguicolors = true
 
 vim.g.tokyonight_style = 'night'
-vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_terminal_colors = false
+vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = true
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_variables = true
 vim.g.tokyonight_transparent = true
+vim.g.tokyonight_hide_inactive_statusline = false
+vim.g.tokyonight_sidebars = {}
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_dark_sidebar = true
+vim.g.tokyonight_dark_float = true
+vim.g.tokyonight_colors = { bg_float = 'none' }
+vim.g.tokyonight_day_brightness = 0.3
 vim.g.tokyonight_lualine_bold = true
 
 vim.cmd('colorscheme tokyonight')
