2e5b1f2d75a6fd3c94b395fa9c5f9a1fd66d4fb6
anasrar <mynameanasrar@gmail.com>
2022-09-09 02:35:23 +1200

    feat(neovim-theme): change setup to lua function `tokyonight`

--- neovim/.config/nvim/lua/rin/theme/tokyonight.lua
+++ neovim/.config/nvim/lua/rin/theme/tokyonight.lua
@@ -1,25 +1,46 @@
 vim.o.termguicolors = true
 
-vim.g.tokyonight_style = 'night'
-vim.g.tokyonight_terminal_colors = false
-vim.g.tokyonight_italic_comments = true
-vim.g.tokyonight_italic_keywords = true
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_italic_variables = true
-vim.g.tokyonight_transparent = true
-vim.g.tokyonight_hide_inactive_statusline = false
-vim.g.tokyonight_sidebars = {}
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_dark_float = true
-vim.g.tokyonight_colors = { bg_float = 'none' }
-vim.g.tokyonight_day_brightness = 0.3
-vim.g.tokyonight_lualine_bold = true
+require("tokyonight").setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = false, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value `:help attr-list`
+    comments = "italic",
+    keywords = "italic",
+    functions = "italic",
+    variables = "italic",
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "dark", -- style for sidebars, see below
+    floats = "dark", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- fucntion will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors)
+    colors.bg_float = 'none'
+  end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- fucntion will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors)
+    highlights.StatusLineTabActive = { bg= '#e0af68', fg='#15161e' }
+    highlights.StatusLineTabInactive = { bg = '#414868', fg = '#15161e' }
+    highlights.GitSignsAdd = { link = 'String' }
+    highlights.GitSignsChange = { link = 'diffFile' }
+    highlights.GitSignsDelete = { link = 'TSVariableBuiltin' }
+  end,
+})
 
 vim.cmd('colorscheme tokyonight')
-
-vim.api.nvim_set_hl(0, 'StatusLineTabActive', { bg= '#e0af68', fg='#15161e' })
-vim.api.nvim_set_hl(0, 'StatusLineTabInactive', { bg = '#414868', fg = '#15161e' })
-vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'String', })
-vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'diffFile' })
-vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'TSVariableBuiltin' })
