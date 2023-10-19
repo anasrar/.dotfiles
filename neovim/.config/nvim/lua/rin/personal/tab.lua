1ee2d19574cb7ef71f199a653080a9f8aa348b97
anasrar <mynameanasrar@gmail.com>
2023-10-20 08:20:35 +1300

    refactor(neovim+personal+tab): remove status line

--- neovim/.config/nvim/lua/rin/personal/tab.lua
+++ neovim/.config/nvim/lua/rin/personal/tab.lua
@@ -1,25 +1,7 @@
 local M = {}
-local tab_name = {
-  "main",
-  "debug",
-}
-
-M.statusline = function()
-  local result = "";
-  local current_tab = vim.fn.tabpagenr()
-  local total_tab = vim.fn.tabpagenr("$")
-
-  for i = 1, total_tab do
-    local hl = i == current_tab and "StatusLineTabActive" or "StatusLineTabInactive"
-    local name = tab_name[i] or "unknown";
-    result = result .. "  " .. "%#" .. hl .. "Number# " .. i .. " %* %#" .. hl .. "#" .. name .. "%*"
-  end
-
-  return result .. " "
-end
 
 M.setup = function()
-  vim.cmd(string.rep("tab split", #tab_name - 1, "|") .. "|tabfirst")
+  vim.cmd(string.rep("tab split", 1, "|") .. "|tabfirst")
 end
 
 if not pcall(debug.getlocal, 4, 1) then
