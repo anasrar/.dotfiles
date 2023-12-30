68f61916bba70ac1ea4a16a1ceadb896c92e3066
anasrar <mynameanasrar@gmail.com>
2023-12-31 00:21:05 +1300

    refactor(neovim+neo-tree): always show `.cargo`

--- neovim/.config/nvim/lua/rin/filetree/neo-tree.lua
+++ neovim/.config/nvim/lua/rin/filetree/neo-tree.lua
@@ -173,6 +173,7 @@ M.setup = function()
         always_show = { -- remains visible even if other settings would normally hide it
           ".gitignore",
           ".github",
+          ".cargo",
           -- TODO: add more stuff here, like eslint and prettier
         },
         never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
