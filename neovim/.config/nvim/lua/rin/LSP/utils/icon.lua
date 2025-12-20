f156426d82043db90b8e98a35bbd85a3f61a85f4
anasrar <mynameanasrar@gmail.com>
2025-12-20 18:30:52 +0900

    refactor(neovim+LSP): lsp hover rounded

--- neovim/.config/nvim/lua/rin/LSP/utils/icon.lua
+++ neovim/.config/nvim/lua/rin/LSP/utils/icon.lua
@@ -12,12 +12,9 @@ vim.diagnostic.config({
     border = "rounded",
   },
 })
+
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "rounded",
-})
