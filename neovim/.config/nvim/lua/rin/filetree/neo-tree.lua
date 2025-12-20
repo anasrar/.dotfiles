70d75ea58a60c2c1dd30004ed84abaa27521b69e
anasrar <mynameanasrar@gmail.com>
2025-12-20 18:29:41 +0900

    refactor(neovim+neo-tree): hide root node

--- neovim/.config/nvim/lua/rin/filetree/neo-tree.lua
+++ neovim/.config/nvim/lua/rin/filetree/neo-tree.lua
@@ -26,6 +26,7 @@ M.setup = function()
 
   -- # Setup
   neo_tree.setup({
+    hide_root_node = true,
     close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
     popup_border_style = "rounded",
     enable_git_status = true,
