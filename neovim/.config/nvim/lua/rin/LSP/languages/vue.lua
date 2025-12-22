2789809cb0b5233b14b47a082f09414024278e47
anasrar <mynameanasrar@gmail.com>
2025-12-22 14:07:51 +0900

    refactor(neovim+lsp+vue): refactor to use `vim.lsp`

--- neovim/.config/nvim/lua/rin/LSP/languages/vue.lua
+++ neovim/.config/nvim/lua/rin/LSP/languages/vue.lua
@@ -1,20 +1,10 @@
 --[=[
-Vue langue server using volar
+Vue langue server
 ```
-npm i -g @volar/vue-language-server eslint_d·@fsouza/prettierd
+npm i -g @vue/vue-language-server eslint_d·@fsouza/prettierd
 ```
 --]=]
 
-local ok = require("rin.utils.check_requires").check({
-  "lspconfig",
-  "cmp_nvim_lsp",
-  "null-ls",
-})
-if not ok then
-  return
-end
-
-local lspconfig = require("lspconfig")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local null_ls = require("null-ls")
 
@@ -27,7 +17,7 @@ end
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-lspconfig.volar.setup({
+vim.lsp.config("vue_ls", {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
@@ -35,6 +25,7 @@ lspconfig.volar.setup({
     on_attach(client, bufnr)
   end,
 })
+vim.lsp.enable("vue_ls")
 
 null_ls.register({
   name = "null-ls-Vue",
