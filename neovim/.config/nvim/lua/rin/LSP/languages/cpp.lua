b1bf3f4b854cbfba97d86f68ddd8df3c731953ba
anasrar <mynameanasrar@gmail.com>
2025-12-22 14:18:34 +0900

    refactor(neovim+lsp+cpp): refactor to use `vim.lsp`

--- neovim/.config/nvim/lua/rin/LSP/languages/cpp.lua
+++ neovim/.config/nvim/lua/rin/LSP/languages/cpp.lua
@@ -2,16 +2,6 @@
 install clang with bundle clangd and clang-format
 --]=]
 
-local ok = require("rin.utils.check_requires").check({
-  "lspconfig",
-  "cmp_nvim_lsp",
-  "null-ls",
-})
-if not ok then
-  return
-end
-
-local lspconfig = require("lspconfig")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 local null_ls = require("null-ls")
 
@@ -22,13 +12,13 @@ end
 local capabilities
 cmp_nvim_lsp.default_capabilities()
 
-lspconfig.clangd.setup({
+vim.lsp.config("clangd", {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
   end,
-  root_dir = lspconfig.util.root_pattern("compile_commands.json"),
 })
+vim.lsp.enable("clangd")
 
 null_ls.register({
   name = "null-ls-Cpp",
