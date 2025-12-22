8d95197d6a32eba6765b771f4af8051d96f9d938
anasrar <mynameanasrar@gmail.com>
2025-12-22 13:55:59 +0900

    refactor(neovim+lsp): signs

--- neovim/.config/nvim/lua/rin/LSP/utils/icon.lua
+++ neovim/.config/nvim/lua/rin/LSP/utils/icon.lua
@@ -2,7 +2,6 @@ vim.opt.signcolumn = "yes"
 
 vim.diagnostic.config({
   virtual_text = true,
-  signs = true,
   underline = true,
   update_in_insert = false,
   severity_sort = false,
@@ -11,10 +10,24 @@ vim.diagnostic.config({
     source = true,
     border = "rounded",
   },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+    },
+  },
 })
-
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
